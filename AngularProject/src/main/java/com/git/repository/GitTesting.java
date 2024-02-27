a04e12c7aa3800fccbdb9104cddfcdbcb945a282
Fasupport54 <Fasupport54@FASUPPORT52.ind.rsa-ins.com>
2024-02-27 11:29:38 +0530

    git repository

---
+++ AngularProject/src/main/java/com/git/repository/GitTesting.java
@@ -0,0 +1,27 @@
+package com.git.repository;
+
+public class GitTesting {
+
+	private Long token;
+	
+	private String url;
+
+	public Long getToken() {
+		return token;
+	}
+
+	public void setToken(Long token) {
+		this.token = token;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+}
